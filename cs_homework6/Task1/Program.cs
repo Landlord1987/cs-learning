82289b32c1d3a0357299bff08aebd775cf667519
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-17 19:34:16 +0500

    Task1 done

---
+++ cs_homework6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число M: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for(int i = 0; i < n;i++)
+{
+    Console.Write("M{0}: ", i + 1);
+    if (Convert.ToInt32(Console.ReadLine()) > 0) count++;    
+}
+
+Console.Write("Больше нуля -> {0} числа", count);
+
+
+
+
