cd86ec660c77a8bca700d66ac1640d14f3aebd09
Denis Krinitsyn <skylinekrin@gmail.com>
2023-01-06 11:18:43 +0500

    Task1 done

---
+++ cs_homework9/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNum(n);
+
+void PrintNaturalNum(int N)
+{
+    if (N == 1)
+    {
+        Console.WriteLine(N);
+        return;
+    }
+    else
+    {
+        Console.Write(N + " ");
+        PrintNaturalNum(--N);
+    }
+}
