2b0cd0b5685c3450cd56f1bb898b8f8f06576f4e
Denis Krinitsyn <skylinekrin@gmail.com>
2023-01-06 12:04:53 +0500

    Task2 done

---
+++ cs_homework9/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] size = new int[2];
+
+while (true)
+{
+    Console.WriteLine("Задайте значения M и N через пробел: ");
+    size = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+    if (size[0] <= 0 || size[1] <= 0)
+    {
+        Console.WriteLine("Вводите значения только больше 0");
+        continue;
+    }
+    else break;
+}
+
+Console.WriteLine(SumOfElements(size[1]));
+
+int SumOfElements(int elements)
+{
+    int sum = elements;
+    if (elements == size[0]) return elements;
+    else
+    {
+        sum += SumOfElements(--elements);
+    }
+    return sum;
+}
